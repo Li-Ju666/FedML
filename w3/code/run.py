c336df7738330970dec45dc4fee74daa00e4bafd
sariel <li.ju.0122@student.uu.se>
2020-07-20 12:52:32 +0200

    some_modification

--- w3/code/run.py
+++ w3/code/run.py
@@ -10,12 +10,7 @@ from random import sample
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
-
-
-def normalize(data):
-    data_norm = data.astype('float32')
-    data_norm = data_norm/255.0
-    return data_norm
+from matplotlib import pyplot
 
 def load_data():
     (trainX, trainY), (testX, testY) = fashion_mnist.load_data()
@@ -25,6 +20,11 @@ def load_data():
 
     trainY = to_categorical(trainY)
     testY = to_categorical(testY)
+
+    def normalize(data):
+        data_norm = data.astype('float32')
+        data_norm = data_norm / 255.0
+        return data_norm
 
     trainX, testX = normalize(trainX), normalize(testX)
 
@@ -41,16 +41,10 @@ def partition_data(trainX, trainY, num):
 
     return numpy.array(partitionedX), numpy.array(partitionedY)
 
-def save_data(partitionedX, partitionedY, testX, testY):
-    for i in range(0, partitionedX.shape[0]):
-        numpy.savetxt("./data/localX_"+str(i)+".csv", partitionedX[i], fmt="%i", delimiter=",")
-        numpy.savetxt("./data/localY_"+str(i)+".csv", partitionedY[i], fmt="%i", delimiter=',')
-    numpy.savetxt("./data/testX.csv", testX, fmt="%i", delimiter=",")
-    numpy.savetxt("./data/testY.csv", testY, fmt="%i", delimiter=",")
-
 def define_model():
         model = Sequential()
-        model.add(Dense(100, input_dim = 28*28, activation = 'relu', kernel_initializer='he_uniform'))
+        model.add(Dense(100, input_dim = 28*28, activation = 'relu',
+                        kernel_initializer='he_uniform'))
         model.add(Dense(100, activation = 'relu'))
         model.add(Dense(10, activation = 'softmax'))
 
@@ -60,30 +54,28 @@ def define_model():
         return model
 
 
-def fed_learn():
+def fed_learn(trainX, trainY, testX, testY):
     num_clients = 5
-    num_run = 10
-    num_epoch = 5
+    num_run = 20
+    num_epoch = 20
     bs = 10
 
-    trainX, trainY, testX, testY = load_data()
+    ##trainX, trainY, testX, testY = load_data()
     X, Y = partition_data(trainX, trainY, num_clients)
 
     ## build global model
     global_model = define_model()
     global_weights = global_model.get_weights()
+    performance = list()
 
     for run_round in range(0, num_run):
         print("This is run ", run_round)
         local_weights_list = list()
 
         for client in range(0, num_clients):
-            ##print("For client ", client)
             local_model = define_model()
             local_model.set_weights(global_weights)
             local_model.fit(X[client], Y[client], epochs=num_epoch, batch_size=bs, verbose=0)
-            ##_, accuracy = local_model.evaluate(X[client], Y[client], verbose=0)
-            ##print("Accuracy: %.2f"%(accuracy*100))
 
             local_weights_list.append(local_model.get_weights())
 
@@ -91,5 +83,24 @@ def fed_learn():
         global_model.set_weights(global_weights)
         _, accuracy = global_model.evaluate(testX, testY, verbose=0)
         print("Global model accuracy: %.2f" % (accuracy * 100))
+        performance.append(accuracy)
 
-fed_learn()
+def centralized_learn(trainX, trainY, testX, testY):
+    num_epoch = 50
+    bs=10
+    model = define_model()
+    history = model.fit(trainX, trainY, epochs = num_epoch,
+                        batch_size=bs, verbose=1, validation_split=0.2)
+    ##pyplot.title('Classification Accuracy: centralized model')
+    ##pyplot.plot(history.history['accuracy'], color='blue', label='train')
+    ##pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+    ##pyplot.savefig("Centralized.png")
+    _, accuracy = model.evaluate(testX, testY, verbose=0)
+    print("Centralized accuracy: ", accuracy*100)
+
+def run():
+    trainX, trainY, testX, testY = load_data()
+    centralized_learn(trainX, trainY, testX, testY)
+    ##fed_learn(trainX, trainY, testX, testY)
+
+run()
